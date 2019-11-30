e5f9c8b8faed7379a56cf5ff0b8288e9bf054811
DG-Wangtao <taowang@deepglint.com>
2019-11-30 21:40:03 +0800

    feat: load timestamp onloading

--- src/main.js
+++ src/main.js
@@ -1,62 +1,65 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow,Menu,globalShortcut,ipcMain} = require('electron')
-const path = require('path')
+const {app, BrowserWindow, globalShortcut, ipcMain} = require('electron');
+const path = require('path');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
-function createWindow () {
+/**
+ *
+ */
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 60,
     webPreferences: {
-      preload: path.join(__dirname, '/script/preload.js')
+      preload: path.join(__dirname, '/script/preload.js'),
     },
     frame: false,
-    darkTheme:true,
+    darkTheme: true,
     transparent: false,
-    center:true,
-    resizable:false,
+    center: true,
+    resizable: false,
     alwaysOnTop: true,
     skipTaskbar: false,
-    icon: path.join(__dirname, '../assets/timestamp.ico')
-  })
+    icon: path.join(__dirname, '../assets/timestamp.ico'),
+  });
 
   if (process.platform === 'darwin') {
     app.dock.setIcon(path.join(__dirname, '../assets/timestamp.png'));
   }
-  
+
   // and load the index.html of the app.
-  mainWindow.loadFile('./src/index.html')
+  mainWindow.loadFile('./src/index.html');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 
-  mainWindow.on('blur',()=>{
-    if (mainWindow.isVisible()){
-      mainWindow.hide()
+  mainWindow.on('blur', ()=>{
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
     }
-  })
+  });
 
-  mainWindow.on('show',()=>{
-    mainWindow.webContents.send('show')
-  })
+  mainWindow.on('show', ()=>{
+    mainWindow.webContents.send('show');
+  });
 
-  ipcMain.on('esc',(event,message)=>{
-    if (mainWindow.isVisible()){
-      mainWindow.hide()
+  ipcMain.on('esc', (event, message)=>{
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
     }
-  })
+  });
 }
 
 // This method will be called when Electron has finished
@@ -65,31 +68,31 @@ function createWindow () {
 app.on('ready', () => {
   // Menu.setApplicationMenu(null)
 
-  createWindow()
+  createWindow();
 
   // gloabl shortcut
   globalShortcut.register('Alt+Space', () => {
-    if (!mainWindow.isVisible()){
-      mainWindow.show()
+    if (!mainWindow.isVisible()) {
+      mainWindow.show();
     }
-  })
+  });
 
-  app.dock.hide()
-  mainWindow.show()
-})
+  app.dock.hide();
+  mainWindow.show();
+});
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
+  if (mainWindow === null) createWindow();
+});
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
@@ -104,7 +107,8 @@ app.on('activate', function () {
 //     buttons: ['Restart', 'Later'],
 //     title: 'Application Update',
 //     message: process.platform === 'win32' ? releaseNotes : releaseName,
-//     detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+//     detail: 'A new version has been downloaded.
+//      Restart the application to apply the updates.'
 //   }
 
 //   dialog.showMessageBox(dialogOpts, (response) => {
@@ -117,7 +121,8 @@ app.on('activate', function () {
 //   console.error(message)
 // })
 
-//info
+// info
 app.setAboutPanelOptions({
-  "website":"https://github.com/WaylandWong/timestamp-convert"
+  'website': 'https://github.com/WaylandWong/timestamp-convert',
 })
+;
--- src/script/index.js
+++ src/script/index.js
@@ -1,58 +1,89 @@
 'use-strict';
 window.ipcRenderer.on('show', (event, message) => {
-	if(document.activeElement.id != 'output'){
-		input.focus()
-		select()
-	}
-})
+  if (document.activeElement.id != 'output') {
+    input.focus();
+    if (input.value.length==0) {
+      input.value = Date.now();
+    }
+    select();
+  }
+});
 
-function select(){
-	input.select()
+
+/**
+ *
+ */
+function onFocus() {
+  if (input.value.length==0) {
+    input.value = Date.now();
+  }
+  select();
+  valueChange();
 }
 
-function valueChange(){
-	value = input.value
-	if(value.length == 10){
-		try{
-			const timstamp = Number(value);
-		  output.value=getTimeStr(timstamp*1000)
-		}catch(err){
-		}
-	}
-	else if(value.length == 13){
-		try{
-			const timstamp = Number(value);
-			output.value=getTimeStr(timstamp)
-		}catch(err){
-		}
-	}
-	else{
-		output.value=''
-	}
+/**
+ *
+ */
+function select() {
+  input.select();
 }
 
-function keyUp(e) { 
-	var currKey=0,e=e||event; 
-	currKey=e.keyCode||e.which||e.charCode; 
-	
-	if(currKey == 27){
-		if(input.value.length>0 && document.activeElement.id == 'input'){
-		  input.value =''
-			output.value=''
-		}else{
-			window.ipcRenderer.send('esc')
-			return
-		}
-	}
-} 
+/**
+ *
+ */
+function valueChange() {
+  value = input.value;
+  if (value.length == 10) {
+    try {
+      const timstamp = Number(value);
+      output.value=getTimeStr(timstamp*1000);
+    } catch (err) {
+    }
+  } else if (value.length == 13) {
+    try {
+      const timstamp = Number(value);
+      output.value=getTimeStr(timstamp);
+    } catch (err) {
+    }
+  } else {
+    output.value='';
+  }
+}
 
-function getTimeStr(timestamp){
-		const unixTimestamp = new Date(timestamp) 
-		return unixTimestamp.toLocaleString()
+/**
+ *
+ * @param {*} ev
+ */
+function keyUp(ev) {
+  let currKey=0;
+  const e=ev||event;
+  currKey=e.keyCode||e.which||e.charCode;
+
+  if (currKey == 27) {
+    if (input.value.length>0 && document.activeElement.id == 'input') {
+      input.value ='';
+      output.value='';
+    } else {
+      window.ipcRenderer.send('esc');
+      return;
+    }
+  }
+}
+
+
+/**
+ *
+ * get time as string
+ * @param {*} timestamp
+ * @return {void}
+ */
+function getTimeStr(timestamp) {
+  const unixTimestamp = new Date(timestamp);
+  return unixTimestamp.toISOString();
 }
 
 document.onkeyup = keyUp;
-input = document.getElementById('input')
-input.onfocus = select()
-output = document.getElementById('output')
+input = document.getElementById('input');
+input.onfocus = onFocus();
+output = document.getElementById('output');
 input.oninput= valueChange;
