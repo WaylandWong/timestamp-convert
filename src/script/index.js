098316696e4e30f2b5a8dc37fa0c4fd82b2f819c
DG-Wangtao <taowang@deepglint.com>
2020-03-06 23:51:33 +0800

    feat: change date string format  "yyyy-MM-dd hh:mm:ss" with local time zone

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,28 @@
 'use-strict';
+function padding(length, num){
+   return (Array(length).join("0") + num).slice(-length);
+}
+Date.prototype.format = function(fmt) { 
+    var o = { 
+       "M+" : this.getMonth()+1,                 //月份 
+       "d+" : this.getDate(),                    //日 
+       "h+" : this.getHours(),                   //小时 
+       "m+" : this.getMinutes(),                 //分 
+       "s+" : this.getSeconds(),                 //秒 
+       "q+" : Math.floor((this.getMonth()+3)/3), //季度 
+       "S"  : this.getMilliseconds()             //毫秒 
+   }; 
+   if(/(y+)/.test(fmt)) {
+           fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+   }
+    for(var k in o) {
+       if(new RegExp("("+ k +")").test(fmt)){
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+        }
+    }
+   return fmt; 
+}        
+
 
 window.ipcRenderer.on('show', (event, message) => {
     if (document.activeElement.id !== 'output') {
@@ -140,7 +164,7 @@ function uuid(version) {
  */
 function getTimeStr(timestamp) {
     const unixTimestamp = new Date(timestamp);
-    return unixTimestamp.toISOString();
+    return unixTimestamp.format("yyyy-MM-dd hh:mm:ss")
 }
 
 document.onkeyup = keyUp;
